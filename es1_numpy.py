dd3330f1c3c539d3e6c236288bc07f9b0d88fa6f
Danilo-ale <danilo09.alessi@gmail.com>
2024-07-19 11:05:16 +0200

    Add files via upload

--- es1_numpy.py
+++ es1_numpy.py
@@ -3,13 +3,26 @@ Scenario: Un laboratorio scientifico registra le temperature ogni ora.
 Obiettivo: Utilizzare numpy per calcolare la temperatura media,
  minima e massima registrata.
 Dati: Un array numpy di temperature registrate in una giornata.
+
+Determina la temperatura più probabile per le prossime 4 posizioni 
+rispetto a un aumento costante di 0,2 gradi al giorno ogni settimana. 
 """
 
 import numpy as np
-temp=np.array([10,20,30,-3,0,12,13,22])
-
+#temp di un giorno. 24 ore-> 24 temp
+temp = np.random.uniform(15, 30, 24)
+temp=np.round(temp,1)
 mas=np.max(temp)
 min=np.min(temp)
 media=np.mean(temp)
 
 print("Temp massima:",mas,"\nTemp minima: ",min,"\nTemp media:",media)
+
+#temperatura aumenta di 0,2 gradi/giorno
+aumento_ora=0.2/24
+
+#le prossime 4 temperature orarie partono dall'ultima temp[-1]
+temp_pros=np.arange(1,5)*aumento_ora
+#sommo l'ultima temp
+temp_pros+=temp[-1]
+print("Temperature l prossime 4 ore:", temp_pros)
