ab0ee6a01a388165615ec2014bbff8839a66e95c
Danilo-ale <danilo09.alessi@gmail.com>
2024-07-19 12:24:33 +0200

    es1_numpy.py

--- es1_numpy.py
+++ es1_numpy.py
@@ -22,7 +22,8 @@ print("Temp massima:",mas,"\nTemp minima: ",min,"\nTemp media:",media)
 aumento_ora=0.2/24
 
 #le prossime 4 temperature orarie partono dall'ultima temp[-1]
+#array temp_pros da 1 a 4, calcolando l'aumento all'ora di temperature
 temp_pros=np.arange(1,5)*aumento_ora
-#sommo l'ultima temp
+#sommo l'ultima temp all'array
 temp_pros+=temp[-1]
 print("Temperature l prossime 4 ore:", temp_pros)
